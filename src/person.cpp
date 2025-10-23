6d06bfd9bc329e4eef80ad75b64d0aad89643315
Eric G <ericgjinollari@gmail.com>
2025-10-23 19:10:15 -0400

    test

--- src/person.cpp
+++ src/person.cpp
@@ -1,3 +1,22 @@
-#include "person.h"
 #include <string>
+#include "person.h"
 
+std::string Person::get_first(){
+    return first_name;
+}
+
+std::string Person::get_last(){
+    return last_name;
+}
+
+std::string Person::get_origin(){
+    return origin;
+}
+
+std::string Person::get_destination(){
+    return destination;
+}
+
+int Person::get_birthday(){
+    return birthday;
+}
--- src/person.h
+++ src/person.h
@@ -1,10 +1,23 @@
 #pragma once
+
 #include <string>
 
-class Person {
-  private:
-  std::string name;
+class Person{
+    private:
+        
 
-  public:
+    std::string first_name;
+    std::string last_name;
+    int birthday;
+    std::string origin;
+    std::string destination;
+
+
+    public:
+    std::string get_first();
+    std::string get_last();
+    int get_birthday();
+    std::string get_origin();
+    std::string get_destination();
 
 };
