aa800e43ee08d4fc502c17275b055fcd86281261
Eric G <ericgjinollari@gmail.com>
2025-11-04 21:27:51 -0500

    BTreeNode started

--- src/BTree/btreenode.h
+++ src/BTree/btreenode.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <vector>
+#include <string>
 
 class BTreeNode{
-    
+    private:
+    std::vector<std::string> keys;
+    int min_degree;
+    std::vector<BTreeNode*> children;
+    int num_of_keys;
+    bool leaf;
+
+    public:
+
+    virtual void insert_non_full(std::string val) = 0;
+    void split(int i, BTreeNode* child);
+    void traverse();
+    virtual BTreeNode* search(std::string val) = 0;
+
+    std::vector<std::string>& get_keys();
+    int get_min_degree();
+    std::vector<BTreeNode*>& get_children();
+    int get_num_of_keys();
+    bool is_leaf();
+
 };
+
